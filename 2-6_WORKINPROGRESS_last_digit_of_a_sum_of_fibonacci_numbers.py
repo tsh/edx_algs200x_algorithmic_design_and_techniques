e887bfbbff32cc0ee8e2d20f5302242537ca9147
Anton Nikulin <tsh@users.noreply.github.com>
2018-04-29 22:47:40 +0300

    Update 2-6_WORKINPROGRESS_last_digit_of_a_sum_of_fibonacci_numbers.py

--- 2-6_WORKINPROGRESS_last_digit_of_a_sum_of_fibonacci_numbers.py
+++ 2-6_WORKINPROGRESS_last_digit_of_a_sum_of_fibonacci_numbers.py
@@ -4,15 +4,19 @@ import sys
 
 def fibonacci_sum_naive(n):
     """ Calculate fibonacci for n + 2"""
-    cur, prev = 0, 1
-
-    for i in range(n+2):
+    prev, cur = 0, 1
+    fibs = [prev, cur]
+    for i in range(58):
         prev, cur = cur, cur + prev
-    fib_sum = cur - 1
-    return fib_sum % 10
+        fibs.append(cur % 10)
+    print(fibs)
+    return fibs[(n%58)+3] - 1
 
 
 if __name__ == '__main__':
-    input = 63
+    input = 100
     n = int(input)
     print(fibonacci_sum_naive(n))
+    # assert fibonacci_sum_naive(239) == 0
+    # assert fibonacci_sum_naive(100) == 5
+    # assert fibonacci_sum_naive(3) == 4
