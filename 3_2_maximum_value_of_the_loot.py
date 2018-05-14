589c5461d8e178ec86eb44999ef168667e5a67ff
tsh <dr.tallin@gmail.com>
2018-05-14 22:29:24 +0300

    new loot value structure

--- 3_2_maximum_value_of_the_loot.py
+++ 3_2_maximum_value_of_the_loot.py
@@ -1,9 +1,21 @@
-with open('data/3_2_loot.in') as f:
-    n, capacity = map(int, f.readline().split())
-    items = []
-    for line in f.readlines():
-        if len(line) < 2:
-            # empty line
-            continue
-        value, weight = map(int, line.split())
-        items.append((value, weight,))
+def dataset():
+    with open('data/3_2_loot.in') as f:
+        n, capacity = map(int, f.readline().split())
+        items = []
+        for line in f.readlines():
+            if len(line) < 2:
+                # empty line
+                continue
+            value, weight = map(int, line.split())
+            items.append((value, weight,))
+    return capacity, items
+
+
+def loot_value(capacity, items):
+    items.sort(key=lambda x: x[0] / x[1], reverse=True)  # most valuable
+
+    while capacity > 0:
+        pass
+
+if __name__ == '__main__':
+    print(loot_value(50, [(60, 20), (100, 50), (120, 30)]))  #180
