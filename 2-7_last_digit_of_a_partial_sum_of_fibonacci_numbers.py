1fec08fb3517b977b0cb1cbb1cbf59f04af3bed5
tsh <dr.tallin@gmail.com>
2018-05-15 15:17:39 +0300

    2-7

--- 2-7_last_digit_of_a_partial_sum_of_fibonacci_numbers.py
+++ 2-7_last_digit_of_a_partial_sum_of_fibonacci_numbers.py
@@ -5,18 +5,16 @@ import sys
 def fibonacci_partial_sum_naive(from_, to):
     prev, cur = 0, 1
     fibs = [prev, cur]
-    sums = [prev, prev+cur]
+    sums = [cur]
     # patterns repeats after 60 digits
     for i in range(58):
         prev, cur = cur, cur + prev
         fibs.append(cur%10)
-    mto = (to+1) % 60
-    if mto > from_:
-        res = sum(fibs[from_:]) + sum(fibs[:mto])
-    else:
-        res = sum(fibs[from_:mto])
-    print(res)
-    return res % 10
+        sums.append(sums[i] + cur)
+    mto = to % 60
+    print(sums)
+    print(mto, sums[mto])
+    return sums[mto] % 10
 
 
 if __name__ == '__main__':
@@ -24,6 +22,6 @@ if __name__ == '__main__':
     # from_, to = map(int, input.split())
     # print(fibonacci_partial_sum_naive(from_, to))
     # assert fibonacci_partial_sum_naive(3, 7) == 1
-    # assert fibonacci_partial_sum_naive(10, 10) == 5
+    assert fibonacci_partial_sum_naive(10, 10) == 5
     # assert fibonacci_partial_sum_naive(10, 200) == 2
-    assert fibonacci_partial_sum_naive(1234, 12345) == 5
+    # assert fibonacci_partial_sum_naive(1234, 12345) == 5
