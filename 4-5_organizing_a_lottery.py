eda8a52fbcb2be1a14951d6cb52a6df656bf005b
tsh <dr.tallin@gmail.com>
2018-05-20 23:00:05 +0300

    complete lottery

--- 4-5_organizing_a_lottery.py
+++ 4-5_organizing_a_lottery.py
@@ -4,16 +4,34 @@ def dataset():
         segments, points = [], []
         for _ in range(n_segments):
             segments.append(tuple(map(int, f.readline().split())))
-        points.append(list(map(int, f.readline().split())))
+        points.extend(list(map(int, f.readline().split())))
     return segments, points
 
 
 def lottery(segments, points):
-    pass
+    START, END, POINT = 1,2,3
+    data = []
+    result = [0] * len(points)
+    for start, end in segments:
+        data.append((start, START))
+        data.append((end, END))
+    for position, point in enumerate(points):
+        data.append((point, POINT, position))
+    data.sort(key=lambda x: x[0])
+    open_segments = 0
+    for item in data:
+        if item[1] == START:
+            open_segments += 1
+        elif item[1] == END:
+            open_segments -= 1
+        elif item[1] == POINT:
+            result[item[2]] = open_segments
+    return result
+
 
 
 if __name__ == '__main__':
-    print(lottery([(0,5), (7,10)], [1,6,11]))  # 1 0 0
-    print(lottery([(-10, 10)], [-100, 100, 0]))  # 0 0 1
-    print(lottery([(0,5), (-3,2), (7,10)], [1,6]))  # 2 0
-    # print(lottery(*dataset()))
+    # print(lottery([(0,5), (7,10)], [1,6,11]))  # 1 0 0
+    # print(lottery([(-10, 10)], [-100, 100, 0]))  # 0 0 1
+    # print(lottery([(0,5), (-3,2), (7,10)], [1,6]))  # 2 0
+    print(sum(lottery(*dataset())))
